834b9356fe74556ad36f3a0de857a04750c7776e
Timofey Titovets <nefelim4ag@gmail.com>
2019-01-01 16:29:39 +0300

    Refactor app code

--- app.py
+++ app.py
@@ -8,14 +8,22 @@
 # warranty.
 
 from flask import Flask, request, jsonify, Response
-import ldap, ldap.filter, logging, sys
+import sys
+import ldap
+import logging
 
 logger = logging.getLogger('kube-ldap-authn')
 logger.setLevel(logging.INFO)
 hdl = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s [%(levelname)-8s] %(name)-15s: %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s [%(levelname)-8s] %(name)-15s: %(message)s')
 hdl.setFormatter(formatter)
 logger.addHandler(hdl)
+
+
+class Failure(Exception):
+    pass
+
 
 app = Flask('kube-ldap-authn')
 
@@ -29,93 +37,119 @@ if 'LDAP_TLS_CA_FILE' in app.config:
     ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, app.config['LDAP_TLS_CA_FILE'])
     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_DEMAND)
 
+
 @app.route('/healthz', methods=['GET'])
 def healthz():
     return Response("OK\n", mimetype='text/plain')
 
-@app.route('/authn', methods=['POST'])
-def authn():
-    auth_error = jsonify({
-        'apiVersion': 'authentication.k8s.io/v1beta1',
-        'kind': 'TokenReview',
-        'status': {
-            'authenticated': False,
-        },
-    })
 
+AUTH_ERROR = jsonify({
+    'apiVersion': 'authentication.k8s.io/v1beta1',
+    'kind': 'TokenReview',
+    'status': {
+        'authenticated': False,
+    },
+})
+
+
+def check_request_valid(request):
     req = request.json
     if req is None:
-        logger.info("invalid or missing request json")
-        return auth_error
+        raise Failure("invalid or missing request json")
 
-    if 'apiVersion' not in req or req['apiVersion'] != 'authentication.k8s.io/v1beta1':
-        logger.info("invalid or missing apiVersion")
-        return auth_error
-    if 'kind' not in req or req['kind'] != 'TokenReview':
-        logger.info("invalid or missing kind")
-        return auth_error
-    if 'spec' not in req or 'token' not in req['spec']:
-        logger.info("invalid or missing spec")
-        return auth_error
+    for field in ('apiVersion', 'kind', 'spec'):
+        if req.get(field) is None:
+            raise Failure("missing {}".format(field))
 
-    token = req['spec']['token']
+    supported_api = ('authentication.k8s.io/v1beta1')
 
+    if req['apiVersion'] not in supported_api:
+        raise Failure("apiVersion: {} not supported".format(req['apiVersion']))
+
+    if req['kind'] != 'TokenReview':
+        raise Failure("kind: {} not supported".format(req['kind']))
+
+    if req['spec'].get('token'):
+        raise Failure("invalid spec")
+
+
+def get_ldap_connection():
     try:
         ld = ldap.initialize(app.config['LDAP_URL'])
     except ldap.LDAPError as e:
-        logger.info("LDAP connection error: " + str(e))
-        return auth_error
+        raise Failure("LDAP connection error: " + str(e))
 
     ld.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
 
-    if app.config.get('LDAP_START_TLS', True) == True:
+    if app.config.get('LDAP_START_TLS', True):
         try:
             ld.start_tls_s()
         except ldap.LDAPError as e:
-            logger.info("LDAP TLS error: " + str(e))
-            return auth_error
+            raise Failure("LDAP TLS error: " + str(e))
 
     try:
         ld.simple_bind_s(app.config['LDAP_BIND_DN'],
                          app.config['LDAP_BIND_PASSWORD'])
     except ldap.LDAPError as e:
-        logger.info("LDAP bind error: " + str(e))
-        return auth_error
+        raise Failure("LDAP bind error: " + str(e))
 
-    user_search = app.config['LDAP_USER_SEARCH_FILTER'].format(
-                    token=ldap.filter.escape_filter_chars(token))
+    return ld
 
+
+def search_user(ld, user_search):
     try:
         r = ld.search_s(app.config['LDAP_USER_SEARCH_BASE'],
-                        ldap.SCOPE_SUBTREE,
-                        user_search,
-                        [ app.config['LDAP_USER_NAME_ATTRIBUTE'],
-                          app.config['LDAP_USER_UID_ATTRIBUTE'] ])
+                        ldap.SCOPE_SUBTREE, user_search, [
+                            app.config['LDAP_USER_NAME_ATTRIBUTE'],
+                            app.config['LDAP_USER_UID_ATTRIBUTE']
+                        ])
     except ldap.LDAPError as e:
         logger.info("LDAP search error: " + str(e))
-        return auth_error
+        return AUTH_ERROR
 
     if len(r) != 1:
         logger.info("expected 1 user, found " + str(len(r)))
-        return auth_error
+        return AUTH_ERROR
+
+
+@app.route('/authn', methods=['POST'])
+def authn():
+    try:
+        check_request_valid(request.json)
+        ld = get_ldap_connection()
+    except Failure as e:
+        logger.info(e)
+        return AUTH_ERROR
+
+    token = request.json['spec']['token']
+    escaped_token = ldap.filter.escape_filter_chars(token)
+    user_search = app.config['LDAP_USER_SEARCH_FILTER'].format(escaped_token)
+
+    try:
+        r = search_user(ld, user_search)
+    except Failure as e:
+        logger.info(e)
+        return AUTH_ERROR
 
     username = r[0][1][app.config['LDAP_USER_NAME_ATTRIBUTE']][0].decode('ascii')
     uid = r[0][1][app.config['LDAP_USER_UID_ATTRIBUTE']][0].decode('ascii')
 
-    group_search = app.config['LDAP_GROUP_SEARCH_FILTER'].format(
-                        username=ldap.filter.escape_filter_chars(username),
-                        dn=ldap.filter.escape_filter_chars(r[0][0]))
+    username_escaped = ldap.filter.escape_filter_chars(username)
+    dn_escaped = ldap.filter.escape_filter_chars(r[0][0])
+
+    group_search = app.config['LDAP_GROUP_SEARCH_FILTER'].format(username_escaped, dn_escaped)
     try:
         g = ld.search_s(app.config['LDAP_GROUP_SEARCH_BASE'],
                         ldap.SCOPE_SUBTREE, group_search,
-                        [ app.config['LDAP_GROUP_NAME_ATTRIBUTE'] ])
+                        [app.config['LDAP_GROUP_NAME_ATTRIBUTE']])
     except ldap.LDAPError as e:
         logger.info("LDAP search error: " + str(e))
-        return auth_error
+        return AUTH_ERROR
 
     groups = [
-            i[1][app.config['LDAP_GROUP_NAME_ATTRIBUTE']][0].decode('ascii')
-        for i in g ]
+        i[1][app.config['LDAP_GROUP_NAME_ATTRIBUTE']][0].decode('ascii')
+        for i in g
+    ]
 
     return jsonify({
         'apiVersion': 'authentication.k8s.io/v1beta1',
